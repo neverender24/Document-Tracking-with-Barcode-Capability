3e4584b25604276278ea232e633ed502f24e4cf1
neverender24 <neverender24@gmail.com>
2019-05-28 16:32:55 +0800

    print background body to white

--- resources/views/admin/open_pdf.blade.php
+++ resources/views/admin/open_pdf.blade.php
@@ -1,5 +1,11 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+<style>
+body{
+    background-color: #fff;
+}
+</style>
+
 <div class="row">
     <div class="col-lg-6 col-md-6 col-sm-6 text-right">
         <div>
