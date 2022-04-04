ca22481b81fd6808ff8a68f44e361b5ac366e997
George <neverender24@gmail.com>
2022-04-04 16:30:14 +0800

    fix fast traxck

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -353,6 +353,8 @@
 <div id="app">
   <fast-track></fast-track>
 </div>
-<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ mix('js/manifest.js') }}" defer></script>
+<script src="{{ mix('js/vendor.js') }}" defer></script>
+<script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 </html>
